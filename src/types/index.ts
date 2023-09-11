15618e720970a6f31cba6ef74390371ecbb08d0d
masterhand83 <robinmiguelsdj@gmail.com>
2023-09-10 23:25:05 -0600

    añadido el tipo Marca

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type Producto = {
+  key: string;
+  codigo: string;
+  nombre: string;
+  marca: string;
+  stock: number;
+  precio: number;
+};
+
+export type  Marca = {
+  id: number;
+  marca: string
+}
