a82ee08cdf5ccba22fc8de94c70b2de9f77392f4
masterhand83 <robinmiguelsdj@gmail.com>
2023-12-13 23:14:58 -0600

    Refactor PronosticoTable component

--- src/components/PronosticoTable.tsx
+++ src/components/PronosticoTable.tsx
@@ -1,20 +1,10 @@
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableFooter,
-  TableHead,
-  TablePagination,
-  TableRow,
-} from '@mui/material';
 import { useEffect, useState } from 'react';
 import PronosticoActionGroup from './PronosticoTable/PronosticoActionGroup';
 import LoadingPronosticoModal from './PronosticoTable/LoadingPronosticoModal';
 import { getListaProductos } from '../services';
 import { useNavigate } from 'react-router-dom';
 import SearchBar from './utils/SearchBar';
+import TablaProductos from './Table/TablaProductos';
 //const dataSource: readonly Producto[] = productosData;
 
 export function PronosticoTable() {
@@ -24,25 +14,11 @@ export function PronosticoTable() {
     null
   );
   const [searchData, setSearchData] = useState('' as string);
-  const includesSearchData = (producto: Producto) => {
-    return (
-      producto.codigo.includes(searchData) ||
-      producto.marca.includes(searchData) ||
-      producto.nombre.includes(searchData)
-    );
-  };
+  const [isLoading, setIsLoading] = useState(false);
   const onProductoSearch = (data: string) => {
     setSearchData(data);
   };
 
-  const [page, setPage] = useState(0);
-  const rowsPerPage = 5;
-  const handleChangePage = (
-    _event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
-  ) => {
-    setPage(newPage);
-  };
   const openModal = (action: string, producto: Producto) => {
     setSelectedProducto(producto);
     console.log(producto);
@@ -52,14 +28,15 @@ export function PronosticoTable() {
       });
     }
     if (action === 'forecast') {
-      // setOpenPronosticoModal(true);
       navigate('/pagina-pronostico', {
         state: { producto },
       })
     }
   };
   useEffect(() => {
+    setIsLoading(true);
     getListaProductos().then((data) => {
+      setIsLoading(false);
       setTabledata(data);
     });
   }, []);
@@ -69,52 +46,19 @@ export function PronosticoTable() {
       <div className="flex space-x-2 items-center">
         <SearchBar onProductoSearch={onProductoSearch} />
       </div>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 'bold' }}>Código</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Producto</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Marca</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Existencia</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Precio</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Acciones</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableData
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .filter(includesSearchData)
-              .map((producto: Producto) => {
-                return (
-                  <TableRow key={producto.codigo}>
-                    <TableCell>{producto.codigo}</TableCell>
-                    <TableCell>{producto.nombre}</TableCell>
-                    <TableCell>{producto.marca}</TableCell>
-                    <TableCell>{producto.existencias}</TableCell>
-                    <TableCell>$ {producto.precio_unitario}</TableCell>
-                    <TableCell>
-                      <PronosticoActionGroup
-                        producto={producto}
-                        onAction={(action) => openModal(action, producto)}
-                      />
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPage={rowsPerPage}
-                count={tableData.length}
-                page={page}
-                onPageChange={handleChangePage}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
+      <TablaProductos
+        tableData={tableData}
+        displayLength={5}
+        searchData={searchData}
+        loadingContent={isLoading}
+        renderActionButtons={(producto: Producto) => (
+          <PronosticoActionGroup
+            producto={producto}
+            onAction={(action) => openModal(action, producto)}
+          />
+        )}
+
+      />
       <LoadingPronosticoModal
         currentProducto={selectedProducto}
         isOpen={openPronosticoModal}
